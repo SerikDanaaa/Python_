2733b988497a4effd936fcba5846700b934ea419
SerikDanaaa <danaserik160102@gmail.com>
2021-02-08 21:40:49 +0600

    leetcode

--- a.cpp
+++
@@ -1,8 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main(){
-    double a = sqrt(10*(pow(16.55*pow(10,-3),1/3)+pow(16.55*pow(10,-3),-2.75+2)));
-    double b =pow(asin(0.15),2)-abs((16.55*pow(10,-3))+2.75);
-    cout<<a*b;
-}
